1b7fe11068c3b2e9cb24812e9553f34824d3157b
delk711 <delk711@student.pcsstn.net>
2020-01-10 15:28:44 +0000

    finished the code

--- main.cs
+++ main.cs
@@ -2,6 +2,50 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Ordinal Numbers");
+    Console.WriteLine ("Max: ");
+    Console.WriteLine(UInt32.MaxValue);
+    string input = Console.ReadLine();
+
+    try{
+
+      uint max = UInt32.Parse(input);
+
+      for (int i = 1; i <= max; i++)
+      {
+        if (i == 0)
+        {
+          Console.WriteLine(i);
+          continue;
+        }
+        switch(i % 100)
+        {
+          case 11:
+          case 12:
+          case 13:
+          Console.WriteLine($"{i}th");
+          continue;
+        }
+        switch(i % 10) {
+          case 1:
+          Console.WriteLine($"{i}st");
+          continue;
+          case 2:
+          Console.WriteLine($"{i}nd");
+          continue;
+          case 3:
+          Console.WriteLine($"{i}rd");
+          continue;
+          default:
+          Console.WriteLine($"{i}th");
+          continue;
+        }
+      }
+
+    }
+
+    catch(OverflowException)
+    {
+      Console.WriteLine($"Hey, pal... {input} is out of 100");
+    }
   }
 }
